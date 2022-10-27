d7f9f502e03d43d000d4cb55407455750780718a
Admin <Admin@USERTL2.tpsonline.com>
2022-10-27 10:17:26 +0500

    Add all my files

---
+++ Index.js
@@ -0,0 +1,51 @@
+const { query } = require('express')
+const express = require('express')
+const app = express()
+const port = 3002
+const path = require('path')
+// creating middleware
+app.use(express.static('public'))
+
+
+app.get('/u', (req, res) => {
+  res.send('Hello World!')
+
+})
+
+app.get('/img' ,(req,res)=> {
+  let imagePath= path.join(__dirname,'image.jpg');
+  res.sendFile(imagePath)
+})
+
+
+app.get('/dynamicimage' ,(req,res)=> {
+  
+  let imgName= req.query.imagename
+
+  let finalImage= path.join(__dirname, imgName)
+
+  if(imgName)
+  {
+    res.sendFile(finalImage)
+    console.log("not found")
+    //res.send("Not Found");
+
+  } else if(imgName== null){
+    //res.send("not found")
+    //res.sendFile(finalImage);
+    console.log("not found")
+
+  }
+  
+    
+ 
+  
+
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
